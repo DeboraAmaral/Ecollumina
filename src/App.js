e667003d7a28eea98006f61b1e03edc7c644841c
Heitor Ribeiro Mota <heitormota2006@gmail.com>
2023-11-12 23:44:39 -0300

    Enviando projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header.jsx';
+import SectionBelowHeader from './components/SectionBelowHeader.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SectionBelowHeader/>
+    </div>
+  );
+}
+
+export default App;
